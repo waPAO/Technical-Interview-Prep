6adab38cb4b5337a8faa0e460bf83ccb154c533e
Pao <pzsigua@gmail.com>
2022-03-23 10:26:59 -0700

    Added canPlaceFlowers and removeElement

---
+++ canPlaceFlowers.py
@@ -0,0 +1,111 @@
+# Can Place Flowers
+'''
+You have a long flowerbed in which some of the plots are planted, and some are not.
+However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, 
+and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+'''
+
+'''
+Example:
+    Input: flowerbed = [1,0,0,0,1], n = 2
+    Output: false
+'''
+class Solution:
+    '''
+    Pseudocode
+
+    SET count to 0
+    ITERATE over length of flowerbed
+        CHECK if flowerbed at index "i" is equal to 0
+            check if the left plot is empty
+            check if the right plot is empty
+
+            CHECK if both left and right plots are empty
+                SET flowerbed at index "i" to 1
+                INCREMENT count by 1
+    RETURN count is greater than or equal to n
+    '''
+    def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+        count = 0
+        for i in range(len(flowerbed)):
+            # Check if the current plot is empty.
+            if flowerbed[i] == 0:
+                # Check if the left and right plots are empty.
+                empty_left_plot = (i == 0) or (flowerbed[i - 1] == 0)
+                empty_right_plot = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+                
+                # If both plots are empty, we can plant a flower here.
+                if empty_left_plot and empty_right_plot:
+                    flowerbed[i] = 1
+                    count += 1
+                    
+        return count >= n
+    
+
+'''
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+'''
+# First iteration
+'''
+count = 0
+for i in range(5):
+    i = 0
+    flowerbed[0] = 1, 1 != 0
+
+count = 0
+'''
+# Second iteration 
+'''
+count = 0
+for i in range(5):
+    i = 1
+    flowerbed[1] = 0, 0 == 0:
+        empty_left_plot = False ((i != 0) or (flowerbed[i-1] != 0))
+        empty_right_plot = True (flowerbed[i + 1] == 0)
+        
+        # does not enter second if statement
+        
+count = 0
+'''
+# Third iteration
+'''
+count = 0
+for i in range(5):
+    i = 2
+    flowerbed[2] = 0, (0 == 0):
+        empty_left_plot = True ( (flowerbed[i-1] == 0)
+        empty_right_plot = True (flowerbed[i + 1] == 0)
+        
+        #both plots are empty
+        flowerbed[2] = 1
+        count += 1
+        
+count = 1
+'''
+# Fourth iteration
+'''
+count = 1
+for i in range(5):
+    i = 3
+    flowerbed[3] = 0, (0 == 0):
+        empty_left_plot = False ((i != 0) or (flowerbed[i-1] != 0))
+        empty_right_plot = False (i != len(flowerbed) - 1) or (flowerbed[i + 1] != 0)
+        
+        # does not enter second if statement
+        
+count = 1
+'''
+# Final iteration
+'''
+count = 1
+for i in range(5):
+    i = 4
+    flowerbed[4] = 1, (1 != 0)
+
+        
+count = 1
+'''
+# return 1 >= 2 => False
---
+++ removeElement.py
@@ -0,0 +1,75 @@
+'''
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+'''
+def removeElement(self, nums: list[int], val: int) -> int:
+    #set count
+    count = 0
+    #iterate through items
+    for i in range(len(nums)):
+        #check if item is equal to val
+        if nums[i] != val :
+            #if not set nums[count] to nums[i] and increment count by 1
+            nums[count] = nums[i]
+            count +=1
+    return count
+
+
+
+'''
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+'''
+# nums = [3,2,2,3]
+# val = 3
+# count = 0 
+
+# start iteration
+'''
+count = 0
+i = 0
+nums[i] = nums[0] = 3
+nums[i] equals 3 (val)
+count = 0
+nums = [3, 2, 2, 3]
+'''
+
+'''
+count = 0
+i = 1
+nums[i] = nums[1] = 2
+nums[i] does not equal 3 (2 != 3)
+    nums[count] = nums[1]
+    nums[0] = 2
+
+count = 1
+nums = [2, 2, 2, 3]
+'''
+
+'''
+count = 1
+i = 2
+nums[i] = nums[2] = 2
+nums[i] does not equal 3 (2 != 3)
+    nums[count] = nums[2] 
+    nums[1] = 2
+    
+count = 2
+nums = [2, 2, 2, 3]
+'''
+
+'''
+count = 2
+i = 3
+nums[i] = nums[3] = 3
+nums[i] equals 3 (val)
+
+nums = [2, 2, 2, 3]
+'''
+
+# return 2
