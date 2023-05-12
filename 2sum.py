1cab49f35b2ca05921b6dbc3fed4ce5586f2bdd6
Pao <pzsigua@gmail.com>
2023-05-11 22:59:40 -0700

    2sum

---
+++ 2sum.py
@@ -0,0 +1,31 @@
+'''
+Given an array of integers nums and an integer target, return indices 
+of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ex: 
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+'''
+
+'''
+pseudocode
+1. iterate through the lists length
+2. nest a loop where you are 1 num ahead
+3. add the two values and check if they equal the targe
+4. return it if it does.
+
+'''
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
